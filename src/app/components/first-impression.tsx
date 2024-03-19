7c921b2894f6720bd8fb4e36a5e4301d925bb4b8
galcsikgyozo <gyozo@galcsik.hu>
2024-03-19 09:54:41 +0100

    fixed hydration error on motion.div and temporarily removed the bucket provider for the image (will rework this later)

--- src/app/components/first-impression.tsx
+++ src/app/components/first-impression.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Image from 'next/image'
 
 import WordLoop from '@/app/components/ui/word-loop'
-import PortraitImage from '@/app/components/ui/portrait-image'
+import portraitImage from '~/images/gyozo-galcsik-portrait.webp'
 
 const FirstImpression: React.FC = () => {
   return (
@@ -58,7 +59,19 @@ const FirstImpression: React.FC = () => {
           />
         </svg>
       </div>
-      <PortraitImage />
+      <Image
+        src={portraitImage}
+        alt="Portrait of Gyozo Galcsik"
+        width={0}
+        height={0}
+        fill={false}
+        sizes="100vw"
+        loading="eager"
+        fetchPriority="high"
+        priority={true}
+        decoding="async"
+        className="absolute right-0 top-1/2 -z-[1] h-1/2 w-screen transform object-cover @md:h-[464px] @md:w-96 max-md:[--tw-translate-y:calc(-50%+var(--tw-translate-y-offset))] max-md:@sm:[--tw-translate-y-offset:96px] md:-translate-y-1/2"
+      />
     </section>
   )
 }
--- src/app/components/ui/cursor-follower.tsx
+++ src/app/components/ui/cursor-follower.tsx
@@ -6,14 +6,6 @@ import { motion, Point } from 'framer-motion'
 import animateSpinVerySlow from './animateSpinVerySlow.module.scss'
 
 const CursorFollower: React.FC = () => {
-  let isDesktop = false
-  if (typeof navigator !== 'undefined') {
-    isDesktop =
-      !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator?.userAgent
-      ) && /Mac|Win|Linux/i.test(navigator?.platform || '')
-  }
-
   const cursorFollowerRef = useRef<HTMLDivElement>(null)
 
   const [cursorPosition, setCursorPosition] = useState<Point>({ x: 0, y: 0 })
@@ -33,29 +25,26 @@ const CursorFollower: React.FC = () => {
   }, [])
 
   return (
-    (isDesktop && (
-      <>
-        <motion.div
-          ref={cursorFollowerRef}
-          // className="fixed size-48 rounded-full bg-white transition-transform duration-300"
-          className={`${animateSpinVerySlow.style} pointer-events-none fixed -z-20 size-96 origin-center rounded-[100%] bg-body [translate:-50%_-50%] @@:h-[804px] @@:w-[538px]`}
-          initial={{
-            left: 0,
-            top: 0,
-          }}
-          animate={{
-            left: cursorPosition.x,
-            top: cursorPosition.y,
-          }}
-          transition={{
-            ease: 'easeOut',
-            duration: 1,
-          }}
-        />
-        <div className="pointer-events-none fixed left-0 top-0 -z-10 h-screen w-screen bg-background/75 backdrop-blur-[200px]" />
-      </>
-    )) ||
-    null
+    <>
+      <motion.div
+        ref={cursorFollowerRef}
+        // className="fixed size-48 rounded-full bg-white transition-transform duration-300"
+        className={`${animateSpinVerySlow.style} pointer-events-none fixed -z-20 size-96 origin-center rounded-[100%] bg-body [translate:-50%_-50%] @@:h-[804px] @@:w-[538px]`}
+        initial={{
+          left: 0,
+          top: 0,
+        }}
+        animate={{
+          left: cursorPosition.x,
+          top: cursorPosition.y,
+        }}
+        transition={{
+          ease: 'easeOut',
+          duration: 1,
+        }}
+      />
+      <div className="pointer-events-none fixed left-0 top-0 -z-10 h-screen w-screen bg-background/75 backdrop-blur-[200px]" />
+    </>
   )
 }
 
