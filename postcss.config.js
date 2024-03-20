2bcbf590408a3663f7e081223e4eaf8070afb691
galcsikgyozo <gyozo@galcsik.hu>
2024-03-20 20:55:57 +0100

    added cssnano for further minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: ['./src/app/**/*.{ts,tsx}'],
-        safelist: ['img', /(.*)\!(.*)$/, /(.*)\:(.*)$/, /(.*)\[(.*)\](.*)$/],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      },
-    ],
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    '@fullhuman/postcss-purgecss': {
+      content: ['./src/app/**/*.{ts,tsx}'],
+      safelist: ['img', /(.*)\!(.*)$/, /(.*)\:(.*)$/, /(.*)\[(.*)\](.*)$/],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    },
+    cssnano: {},
+  },
 }
