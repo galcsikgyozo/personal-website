6b90f6c234068572dc502e2975f83e57afee9600
galcsikgyozo <gyozo@galcsik.hu>
2024-04-04 22:33:52 +0200

    fix: exchanged CursorFollower JS api to a CSS transition because of performance issues on Safari

--- src/app/components/ui/cursor-follower.tsx
+++ src/app/components/ui/cursor-follower.tsx
@@ -1,27 +1,37 @@
 'use client'
 
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useLayoutEffect, useState } from 'react'
 
 const CursorFollower: React.FC = () => {
-  const cursorFollowerRef = useRef<HTMLDivElement>(null)
+  var isDesktop = false
+  // var isSafari = false
 
-  const handleMouseMove = (event: any) => {
-    cursorFollowerRef.current?.animate(
-      {
-        transform: `translate(${event.clientX}px, ${event.clientY}px)`,
-      },
-      {
-        duration: 5000,
-        fill: 'forwards',
-      }
-    )
+  if (typeof navigator !== 'undefined') {
+    isDesktop =
+      !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator?.userAgent
+      ) && /Mac|Win|Linux/i.test(navigator?.platform || '')
+
+    // isSafari = /^((?!chrome|android).)*safari/i.test(navigator?.userAgent)
   }
 
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove)
+  const cursorFollowerRef = useRef<HTMLDivElement>(null)
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
+
+  const handleMouseMove = (event: PointerEvent) => {
+    if (cursorFollowerRef.current) {
+      setCursorPosition({ x: event.clientX, y: event.clientY })
+    }
+  }
+
+  useLayoutEffect(() => {
+    if (!isDesktop) return
+    // if (isSafari) return
+
+    document.addEventListener('pointermove', handleMouseMove)
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('pointermove', handleMouseMove)
     }
   }, [])
 
@@ -30,7 +40,11 @@ const CursorFollower: React.FC = () => {
       <div
         id="cursor-follower"
         ref={cursorFollowerRef}
-        className={`pointer-events-none fixed -z-20 origin-center -rotate-45 scale-y-150 rounded-full bg-body [translate:-50%_-50%] @@:size-[538px]`}
+        className={`pointer-events-none fixed -z-20 origin-center -rotate-45 scale-y-150 rounded-full bg-body transition-all duration-[3s] ease-out-cubic will-change-transform [translate:-50%_-50%] @@:size-[512px]`}
+        style={{
+          left: cursorPosition.x,
+          top: cursorPosition.y,
+        }}
       />
       <div className="pointer-events-none fixed left-0 top-0 -z-10 h-screen w-screen bg-background/75 backdrop-blur-[200px]" />
     </>
