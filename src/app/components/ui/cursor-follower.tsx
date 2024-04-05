654beab3421c8f3f9af7e6e5314f292dda9e9368
galcsikgyozo <gyozo@galcsik.hu>
2024-04-05 15:13:06 +0200

    fix: more performance upgrades and code cleanup for CursorFollower

--- src/app/components/ui/cursor-follower.tsx
+++ src/app/components/ui/cursor-follower.tsx
@@ -1,37 +1,19 @@
 'use client'
 
-import React, { useRef, useLayoutEffect, useState } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 
 const CursorFollower: React.FC = () => {
-  var isDesktop = false
-  // var isSafari = false
-
-  if (typeof navigator !== 'undefined') {
-    isDesktop =
-      !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator?.userAgent
-      ) && /Mac|Win|Linux/i.test(navigator?.platform || '')
-
-    // isSafari = /^((?!chrome|android).)*safari/i.test(navigator?.userAgent)
-  }
-
-  const cursorFollowerRef = useRef<HTMLDivElement>(null)
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 })
 
-  const handleMouseMove = (event: PointerEvent) => {
-    if (cursorFollowerRef.current) {
-      setCursorPosition({ x: event.clientX, y: event.clientY })
-    }
+  const handleMouseMove = (event: MouseEvent) => {
+    setCursorPosition({ x: event.clientX, y: event.clientY })
   }
 
   useLayoutEffect(() => {
-    if (!isDesktop) return
-    // if (isSafari) return
-
-    document.addEventListener('pointermove', handleMouseMove)
+    document.addEventListener('mousemove', handleMouseMove)
 
     return () => {
-      document.removeEventListener('pointermove', handleMouseMove)
+      document.removeEventListener('mousemove', handleMouseMove)
     }
   }, [])
 
@@ -39,14 +21,12 @@ const CursorFollower: React.FC = () => {
     <>
       <div
         id="cursor-follower"
-        ref={cursorFollowerRef}
-        className={`pointer-events-none fixed -z-20 origin-center -rotate-45 scale-y-150 rounded-full bg-body transition-all duration-[3s] ease-out-cubic will-change-transform [translate:-50%_-50%] @@:size-[512px]`}
+        className={`pointer-events-none fixed -z-20 rounded-full bg-body transition-transform duration-[3s] ease-out-cubic [translate:-50%_-50%] size-[512px] blur-[192px]`}
         style={{
-          left: cursorPosition.x,
-          top: cursorPosition.y,
+          transform: `translate3d(${cursorPosition.x}px, ${cursorPosition.y}px, 0) rotate(-45deg) scaleY(1.5)`,
         }}
       />
-      <div className="pointer-events-none fixed left-0 top-0 -z-10 h-screen w-screen bg-background/75 backdrop-blur-[200px]" />
+      <div className="pointer-events-none fixed left-0 top-0 -z-10 h-screen w-screen bg-background/75" />
     </>
   )
 }
