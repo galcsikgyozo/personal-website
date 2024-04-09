0f7dd0b5c66aa8343610f8457be0105e5061eb4e
galcsikgyozo <gyozo@galcsik.hu>
2024-04-09 10:59:40 +0200

    refactor: revert 6a8634a88756233cc19169decb925eed2faca766

--- src/app/components/first-impression.tsx
+++ src/app/components/first-impression.tsx
@@ -41,6 +41,7 @@ const FirstImpression: React.FC = () => {
       <div
         className="first-impression-helper label w-container absolute left-1/2 flex -translate-x-1/2 transform flex-col items-center justify-center text-body @sm:bottom-16 @sm:gap-y-2 @md:bottom-24 @md:gap-y-3 @lg:bottom-16 @lg:gap-y-3"
         role="presentation"
+        aria-hidden="true"
       >
         <span className="first-impression-helper-text pointer-events-none whitespace-nowrap">
           Scroll down for more information
